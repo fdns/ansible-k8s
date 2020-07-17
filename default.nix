09a92937db01dde9e6cc8d68769988e3ddab53c3
Felipe Espinoza <github@espinoza.dev>
2020-07-17 18:55:13 -0400

    Setup cloudflare dns

---
+++ default.nix
@@ -0,0 +1,15 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "pip-env";
+  buildInputs = [
+    # System requirements.
+    readline
+
+    # Python requirements (enough to get a virtualenv going).
+    python3Full
+    python3Packages.boto
+    python3Packages.ansible
+  ];
+  src = null;
+}
